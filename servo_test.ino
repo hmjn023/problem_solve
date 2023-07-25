2afee674d84e60b218e3ba974708fabae171417b
SHase964 <hs6-gottwind.dosu26@outlook.jp>
2023-07-25 13:39:05 +0900

    hoge

--- servo_test.ino
+++ servo_test.ino
@@ -12,52 +12,56 @@ const int switchPinB = 3;
 
 const int servoPinA = 9;
 const int servoPinB = 11;
-const int servoPinC= 13;
+const int servoPinC = 13;
 
 int joyStickPositionA;
 int joyStickPositionB;
 
+void setup()
+{
+	pinMode(joyStickPinA, INPUT);
+	pinMode(joyStickPinB, INPUT);
 
-void setup() {
-  pinMode(joyStickPinA, INPUT);
-  pinMode(joyStickPinB, INPUT);
+	pinMode(switchPinA, INPUT_PULLUP);
+	pinMode(switchPinB, INPUT_PULLUP);
 
-  pinMode(switchPinA, INPUT_PULLUP);
-  pinMode(switchPinB, INPUT_PULLUP);
+	servoA.attach(servoPinA);
+	servoB.attach(servoPinB);
+	servoC.attach(servoPinC);
 
-  servoA.attach(servoPinA);
-  servoB.attach(servoPinB);
-  servoC.attach(servoPinC);
-
-  Serial.begin(9600);
+	Serial.begin(9600);
 }
 
-void loop() {
-	//joyStickPositionA = analogRead(joyStickPinA) / (1023/180);
-	//joyStickPositionB = analogRead(joyStickPinB) / (1023/180);
+void loop()
+{
+	joyStickPositionA = analogRead(joyStickPinA) / (1023 / 180);
+	joyStickPositionB = analogRead(joyStickPinB) / (1023 / 180);
 
-  joyStickPositionA = analogRead(joyStickPinA);
-  joyStickPositionB = analogRead(joyStickPinB);
-  
+	// joyStickPositionA = analogRead(joyStickPinA);
+	// joyStickPositionB = analogRead(joyStickPinB);
+
 	servoA.write(joyStickPositionA);
 	servoB.write(joyStickPositionB);
 
-	bool switchStateA=digitalRead(switchPinA);
-	bool switchStateB=digitalRead(switchPinB);
+	bool switchStateA = digitalRead(switchPinA);
+	bool switchStateB = digitalRead(switchPinB);
 
-  //Serial.println(switchStateA);
-  //Serial.println(switchStateB);
+	// Serial.println(switchStateA);
+	// Serial.println(switchStateB);
 
-  //Serial.println(joyStickPositionA);
-  //Serial.println(joyStickPositionB);
-	
-	if(switchStateA==1 && switchStateB==1){
+	// Serial.println(joyStickPositionA);
+	// Serial.println(joyStickPositionB);
+
+	if (switchStateA == 1 && switchStateB == 1)
+	{
 		servoC.write(90);
 	}
-	else if(switchStateA==0 && switchStateB==1){
+	else if (switchStateA == 0 && switchStateB == 1)
+	{
 		servoC.write(0);
 	}
-	else if(switchStateA==1 && switchStateB==0){
+	else if (switchStateA == 1 && switchStateB == 0)
+	{
 		servoC.write(180);
 	}
 	delay(100);
